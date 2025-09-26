49350de10a3918892cd6ef538775f293c0e6c7ed
Marko Cevrljakovic <marecevrljakovic@gmail.com>
2025-09-26 17:23:34 +0200

    Implemented AOP (Aspect Oriented Programming) for logging

---
+++ src/main/java/dev/marko/MedRecords/aspect/LoggingAspect.java
@@ -0,0 +1,36 @@
+package dev.marko.MedRecords.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(* dev.marko.services.*)")
+    public void serviceMethods() {}
+
+    @Before("serviceMethods()")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Before method: " + joinPoint.getSignature().getName());
+    }
+
+    @After("serviceMethods()")
+    public void logAfter(JoinPoint joinPoint) {
+        System.out.println("After method: " + joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        System.out.println("Method " + joinPoint.getSignature().getName() + " returned: " + result);
+    }
+
+    @AfterThrowing(pointcut = "serviceMethods()", throwing = "ex")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+        System.out.println("Method " + joinPoint.getSignature().getName() + " threw exception: " + ex.getMessage());
+    }
+
+
+
+}
